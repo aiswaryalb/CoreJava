705360600450c9d6b15c77f32d702ce44fcd16fb
Aiswarya Bhaseendran Pillai <aiswaryalb@gmail.com>
2018-09-11 15:33:27 +0530

    Add files via upload

---
+++ Parameter/Maths.java
@@ -0,0 +1,12 @@
+package Parameter;
+import Arithmetic.*;
+class Maths
+{
+	public static void main(String[] args) {
+		
+		Calculator.add(4,2);
+		Calculator.subtract(6,2);
+		Calculator.multiply(2,3);
+		Calculator.divide(6,2);
+	}
+}
