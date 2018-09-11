6a553df2a13a452ba865b2bfaa172e9102ae434e
Aiswarya Bhaseendran Pillai <aiswaryalb@gmail.com>
2018-09-11 16:29:19 +0530

    Add files via upload
    Swing project

---
+++ SchoolProject/src/schoolproject/dbcon.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolproject;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author Aiswarya
+ */
+public class dbcon {
+    
+    Connection con;
+    PreparedStatement stat;
+    ResultSet rs;
+    
+    public Connection getcon()
+    {
+        try
+        {
+        Class.forName("com.mysql.jdbc.Driver");
+        con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/school","root","");
+        }
+        catch(Exception e){
+         e.printStackTrace();
+        }
+        return con;
+        
+        
+        
+    }
+   
+    
+}
